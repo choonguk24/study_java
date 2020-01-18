7439740e9cd62d1ec12a2201c651d288b408d2c7
kim <451121475@qq.com>
2020-01-18 18:28:56 +0900

    .

---
+++ Demo3.java
@@ -0,0 +1,16 @@
+public class Demo3 {
+    public static void main(String[] args) throws InterruptedException {
+        StopThread thread = new StopThread();
+        thread.start();
+        // 休眠一秒，確保i變量自增成功
+        Thread.sleep(1000);
+        // 暂停线程
+        // thread.stop(); // 错误的终止
+        thread.interrupt(); // 正确终止
+        while (thread.isAlive()) {
+            // 确保线程已经终止
+        } // 输出结果
+        thread.print();
+
+    }
+}
--- Thread.md
+++ Thread.md
@@ -6,3 +6,9 @@
 5. Timed Watting
 ### 线程状态切换
 ![img](https://img-blog.csdnimg.cn/20181120173640764.jpeg?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3BhbmdlMTk5MQ==,size_16,color_FFFFFF,t_70)
+
+### 線程終止
+- 不正確的線程終止-Stop  
+Stop: 終止線程，並且清除監控器鎖的信息，但是可能導致線程安全問題。
+
+interrupt ：正確的終止方式。
